cb640ff3c05980b04e0a89c399a52a33e8fd0b2d
Diana Cardona <dscardona@gmail.com>
2023-02-15 14:36:20 -0500

    Fix bug in DirectoryScreen component

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Main from './screens/MainComponent';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+  return <Main />;
+};
