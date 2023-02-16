e999a100e2ce1df85f7e4e8e45b80b3bd50dd7f7
Diana Cardona <dscardona@gmail.com>
2023-02-16 13:54:35 -0500

    Add stack navigation

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,29 +1,31 @@
-import { FlatList } from 'react-native';
-import { Avatar, ListItem } from 'react-native-elements';
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Avatar, ListItem } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
 
-const DirectoryScreen = (props) => {
-    const renderDirectoryItem = ({ item: campsite }) => {
-        return (
-            <ListItem onPress={() => props.onPress(campsite.id)}>
-                <Avatar source={campsite.image} rounded />
-                <ListItem.Content>
-                    <ListItem.Title>
-                        {campsite.name}
-                    </ListItem.Title>
-                    <ListItem.Subtitle>
-                        {campsite.description}
-                    </ListItem.Subtitle>
-                </ListItem.Content>
-            </ListItem>
-        );
-    };
+const DirectoryScreen = ({ navigation }) => {
+  const [campsites, setCampsites] = useState(CAMPSITES);
+
+  const renderDirectoryItem = ({ item: campsite }) => {
     return (
-        <FlatList 
-            data={props.campsites} 
-            renderItem={renderDirectoryItem} 
-            keyExtractor={(item) => item.id.toString()}
-        />
+      <ListItem
+        onPress={() => navigation.navigate("CampsiteInfo", { campsite })}
+      >
+        <Avatar source={campsite.image} rounded />
+        <ListItem.Content>
+          <ListItem.Title>{campsite.name}</ListItem.Title>
+          <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
     );
+  };
+  return (
+    <FlatList
+      data={campsites}
+      renderItem={renderDirectoryItem}
+      keyExtractor={(item) => item.id.toString()}
+    />
+  );
 };
 
 export default DirectoryScreen;
