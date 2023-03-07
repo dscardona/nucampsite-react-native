841abe6da963941ddad3df7f41f1b5145b73e7aa
Diana Cardona <dscardona@gmail.com>
2023-03-07 14:46:41 -0500

    Add LoginScreen with form component

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,4 +1,3 @@
-// export const baseUrl = 'http://192.168.1.79:3001/';
+export const baseUrl = "http://192.168.1.79:3001/";
 
-export const baseUrl = 'http://192.168.1.161:3001/'; 
-
+// export const baseUrl = 'http://192.168.1.161:3001/';
